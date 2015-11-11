e37df106c6790535ef409796037aeb543821b0e4
NartimHenriksen <martin_oensker_sig_en_ipod@hotmail.com>
2015-11-11 12:18:08 +0100

    ready for parser

---
+++ src/project1/NewickPrep.java
@@ -0,0 +1,20 @@
+package project1;
+
+
+import newick.NewickParser;
+
+import newick.NewickParser.*;
+import newick.ParseException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+/**
+ * Created by ballololz on 11/11/2015.
+ */
+public class NewickPrep {
+
+    public NewickPrep(){}
+
+}
