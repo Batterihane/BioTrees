fa95c26936ec538a908e9e9b118d05b8d29f8e5d
Lenaq <nikolajskipperrasmussen@gmail.com>
2015-11-11 13:32:00 +0100

    Forester ex2

--- src/project1/Test.java
+++ src/project1/Test.java
@@ -6,9 +6,12 @@ import org.forester.io.parsers.PhylogenyParser;
 import org.forester.io.parsers.util.ParserUtils;
 import org.forester.phylogeny.Phylogeny;
 import org.forester.phylogeny.PhylogenyMethods;
+import org.forester.phylogeny.PhylogenyNode;
+import org.forester.phylogeny.iterators.PhylogenyNodeIterator;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Thomas on 06-11-2015.
@@ -25,11 +28,10 @@ public class Test {
 
 
     public static void main(String[] args) throws IOException{
-        System.out.println("hej");
         String nhx = "(mammal,(turtle,rayfinfish,(frog,salamander)))";
         Phylogeny phylogeny = new Phylogeny();
 
-        final File treefile = new File("C:\\Users\\Nikolaj\\BioTreeAndSeq\\BioTrees\\trees\\quickTree\\kalign.new");
+        final File treefile = new File("C:\\Users\\Nikolaj\\BioTreeAndSeq\\BioTrees\\trees\\quickTree\\testnewick.new");
         PhylogenyParser parserDependingOnFileType = ParserUtils.createParserDependingOnFileType(treefile, true);
 
         Phylogeny[] phys = null;
@@ -42,8 +44,21 @@ public class Test {
         //org.forester.phylogeny.Phylogeny.class.newInstance().
         //Phylogeny ph = org.forester.phylogeny.Phylogeny.class.newInstance().
         Phylogeny tree = phys[0];
-        MainFrame application = Archaeopteryx.createApplication(tree);
+        PhylogenyNode root = tree.getRoot();
+        System.out.println(tree.isRerootable());
+        //tree.reRoot(tree.getNode(0));
+        //tree.reRoot(tree.getNode("turtle"));
+        PhylogenyNodeIterator phylogenyNodeIterator = tree.iteratorPreorder();
 
+/*
+        while(phylogenyNodeIterator.hasNext()) {
+            System.out.println(phylogenyNodeIterator.next().toString());
+        }
+*/
+
+        List<PhylogenyNode> allDescendants = tree.getNode(0).getAllDescendants();
+        allDescendants.forEach(node -> System.out.println(node.getName()));
+        MainFrame application = Archaeopteryx.createApplication(tree);
 
     }
 
