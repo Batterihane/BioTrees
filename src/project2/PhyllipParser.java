93c74da07c8a6e8a0d679a9f494b763bc0fa9a7b
NartimHenriksen <martin_oensker_sig_en_ipod@hotmail.com>
2015-11-25 12:05:52 +0100

    done parser phyllip

--- src/project2/PhyllipParser.java
+++ src/project2/PhyllipParser.java
@@ -1,7 +1,50 @@
 package project2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by Thomas on 19-11-2015.
  */
-public class PhyllipParser {
+public class PhyllipParser{
+    BufferedReader br;
+
+    public PhyllipParser(String filePath) {
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+        } catch (FileNotFoundException fnf) {
+            System.out.printf("File was not found!");}
+
+    }
+
+    public Tuple<double[][],String[]> parse(){
+
+        double[][] res1=null;
+        String[] res2=null;
+        try {
+            int n = Integer.parseInt(br.readLine());
+            res1 = new double[n][n];
+            res2 = new String[n];
+            for (int i = 0; i < n; i++) {
+                String[] parts = br.readLine().split(" ");
+
+                res2[i] = parts[0];
+
+                for (int j = 1; j < n + 1; j++) {
+                    res1[i][j - 1] = Double.parseDouble(parts[j]);
+                }
+            }
+        }catch(IOException ioe){
+            System.out.println("IOException i PhyllipParser. Fix jeres filer");
+
+        }
+
+
+
+        return new Tuple<double[][],String[]>(res1,res2);
+
+
+    }
 }
--- src/project2/Runner.java
+++ src/project2/Runner.java
@@ -1,11 +1,20 @@
 package project2;
 
+import java.io.IOException;
+
 /**
  * Created by ballololz on 11/19/2015.
  */
 public class Runner {
     public static void main(String[] args) {
-        PhyllipParser pp = new PhyllipParser("matrices//test1.phy");
+        PhyllipParser pp = new PhyllipParser("src//test.phy");
+        Tuple<double[][], String[]> result=null;
+
+        result = pp.parse();
+
+
+        System.out.println(result.getLeft()[4][1]);
+        System.out.println(result.getRight()[3]);
 
     }
 }
