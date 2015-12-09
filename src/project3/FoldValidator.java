76ba06c78c88773a98571b1bd691d46d8df46a02
Lenaq <nikolajskipperrasmussen@gmail.com>
2015-12-09 14:43:02 +0100

    Added Validator

---
+++ src/project3/FoldValidator.java
@@ -0,0 +1,61 @@
+package project3;
+
+public class FoldValidator
+{
+    char[][] foldMatrix;
+    String fold;
+    String hpString;
+
+    public FoldValidator(String hpString, String fold) {
+        int size = 2*fold.length() + 1;
+        this.foldMatrix = new char[size][size];
+        this.fold = fold;
+        this.hpString = hpString;
+    }
+
+    public boolean validate()
+    {
+        if (hpString.length() != (fold.length()+1)) return false;
+
+        int indexX = fold.length();
+        int indexY = fold.length();
+        foldMatrix[indexX][indexY] = hpString.charAt(0);
+
+        for (int i = 0; i < fold.length(); i++)
+        {
+            switch (fold.charAt(i)) {
+                case 'e' :
+                    indexX += 1;
+                    break;
+                case 'w' :
+                    indexX -= 1;
+                    break;
+                case 'n' :
+                    indexY += 1;
+                    break;
+                case 's' :
+                    indexY -= 1;
+                    break;
+            }
+
+            if (foldMatrix[indexX][indexY] == '\u0000') foldMatrix[indexX][indexY] = hpString.charAt(i);
+            else return false;
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        char[] chars = new char[10];
+        System.out.println("flfrrflffrrflrrlf".length());
+        System.out.println("ennesseeeswwswnww".length());
+        System.out.println("hhppppphhppphppphp".length());
+
+        System.out.println((chars[5] == '\u0000'));
+
+        FoldValidator foldValidator = new FoldValidator("hhppppphhppphppphp", "eeeswnnnesseeesww");
+        System.out.println(foldValidator.validate());
+
+    }
+}
