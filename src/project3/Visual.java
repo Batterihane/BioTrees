fea9b2eea7eec4e00c86710cf9cdcfdf23cfd0e5
NartimHenriksen <martin_oensker_sig_en_ipod@hotmail.com>
2015-12-11 12:20:49 +0100

    forgot some stuff

--- src/project3/Visual.java
+++ src/project3/Visual.java
@@ -11,10 +11,12 @@ import java.io.IOException;
 public class Visual {
 
     public static void main(String[] args) {
+        String writeLocation = "project3/visual/RUN.bat";
         FileWriter fw=null;
         CFolder cfold = new CFolder();
 
-        String outp = "";
+        String vis_outp = "";
+        String res_outp = "";
 
         String[] inps = {
                 "hhppppphhppphppphp",
@@ -34,27 +36,35 @@ public class Visual {
                 "ppphhpphhhhpphhhphhphhphhhhpppppppphhhhhhpphhhhhhppppppppphphhphhhhhhhhhhhpphhhphhphpphphhhpppppphhh"};
 
         ScoreFinder scofi = new ScoreFinder();
+        ////code for running the visualizer
         for(int i=0;i<inps.length;i++){
 
             String s = cfold.fold(inps[i]);
             int score = scofi.findScore(inps[i],s);
-            outp+=(String.format("py -2 hpview.py %s %s > newfolds/fold%d_score%d.txt\n", inps[i],Abs2rel.convert(s),i+1,score));
+            vis_outp+=(String.format("py -2 hpview.py %s %s > newfolds/fold%d_score%d.txt\n", inps[i],Abs2rel.convert(s),i+1,score));
 
         }
 
+        /////Code for writing results.txt
+        for(int i=0;i<inps.length;i++){
+            String s = cfold.fold(inps[i]);
+            int score = scofi.findScore(inps[i],s);
+            res_outp+=(String.format("%d: %s %s -%d\r\n", i+1,inps[i],Abs2rel.convert(s),score));
+
+        }
+
+
         try
         {
-            fw = new FileWriter("project3/visual/RUN.bat");
-            fw.write(outp);
+            fw = new FileWriter("project3/visual/vis.bat");
+            fw.write(vis_outp);
+            fw.close();
+            fw = new FileWriter("project3/visual/results.txt");
+            fw.write(res_outp);
             fw.close();
         }catch(IOException ioe){
             System.out.printf("ioerror");
         }
-
-
-        for (int i = 0;i<inps.length;i++)//print scores til sammenligning
-            System.out.println("fold "+i+" score: "+ scofi.findScore(inps[i],cfold.fold(inps[i])));
-
 
 
 
